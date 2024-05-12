a684c1a8a70ed78e6afaee5bdb50670b4d96a1ee
Mykola Lebid <realkolos1@gmail.com>
2024-05-12 19:40:03 +0300

    basic pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,51 @@
 import React from "react";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const APP_NAME = "SiloMaster";
+const APP_DEFAULT_TITLE = "Керуйте зовнішніми силосами в STEP7 - SiloMaster";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION =
+  "Ефективно керуйте зовнішніми силосами з SiloMaster. Моніторинг дозування, втручання за потреби та оптимізація керування партіями. Натисніть, щоб оптимізувати ваш виробництво вже сьогодні!";
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
+};
+
 export const metadata: Metadata = {
-  title: "Керуйте зовнішніми силосами в STEP7 - SiloMaster",
-  description:
-    "Ефективно керуйте зовнішніми силосами з SiloMaster. Моніторинг дозування, втручання за потреби та оптимізація керування партіями. Натисніть, щоб оптимізувати ваш виробництво вже сьогодні!",
+  title: APP_DEFAULT_TITLE,
+  description: APP_DESCRIPTION,
+  applicationName: APP_NAME,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
 
 export default function RootLayout({
